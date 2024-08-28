355766e1478843d6a17f04a89a92d7c9e45de9b3
soaryong <soaryong.c@gmail.com>
2024-08-28 20:56:03 +0900

    metadata

--- app/(wnw)/layout.tsx
+++ app/(wnw)/layout.tsx
@@ -1,10 +1,17 @@
 import Header from '@/components/layout/header';
 import type { Metadata } from 'next';
-import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Wise and Weird',
-  description: 'Wise and Weird - Prediction Market'
+  description: 'Wise and Weird - Prediction Market',
+  twitter: {
+    card: 'summary',
+    site: '@web3BlueNode',
+    title: 'Wise and Weird',
+    description:
+      'Predict price movement and profit from the community powered official events, making precise decisions',
+    images: 'https://bnb-wnw.online/twitter-img.png'
+  }
 };
 
 export default function DashboardLayout({
@@ -14,24 +21,6 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <Head>
-        <title>Wise And Weird</title>
-        <meta
-          name="description"
-          content="Get precise news and predict the price"
-        />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@web3BlueNode" />
-        <meta name="twitter:title" content="Wise And Weird" />
-        <meta
-          name="twitter:description"
-          content="Predict price movement and profit from the community powered official events, making precise decisions"
-        />
-        <meta
-          name="twitter:image"
-          content="https://bnb-wnw.online/twitter-img.png"
-        />
-      </Head>
       <Header />
       <div className="flex h-screen overflow-hidden">
         <main className="flex-1 overflow-hidden pt-20">
