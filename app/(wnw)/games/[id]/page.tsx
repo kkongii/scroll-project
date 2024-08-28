1666d2acda452eb279d24b11ff9d6d03abb19d54
chan3785 <97221124+chan3785@users.noreply.github.com>
2024-08-28 22:56:58 +0900

    sns

--- app/(wnw)/games/[id]/page.tsx
+++ app/(wnw)/games/[id]/page.tsx
@@ -9,6 +9,7 @@ import { useReadContract } from 'wagmi';
 import WNW_ABI from '@/abi/IWNW.abi';
 import { useSearchParams } from 'next/navigation';
 import { tokenInfos } from '@/constants';
+import Head from 'next/head';
 
 export default function Page() {
   const [gameTitle, setGameTitle] = useState('');
@@ -19,6 +20,17 @@ export default function Page() {
   const WNW_PRECOMPILE_ADDRESS = '0xd38aa26b0b558c19c61c3944ae87bb65786f425d';
   const searchParams = useSearchParams();
   const key = searchParams.get('key');
+  const url = 'https://bnb-wnw.online/';
+  const text = `ADF referral share ${url}`;
+  const encodedText = encodeURIComponent(text);
+  const encodedUrl = encodeURIComponent(url);
+
+  const shareTwitter = () => {
+    const via = 'Wise and Weird';
+    const hashtags = 'Prediction,Price,BNB,BSC';
+    const shareUrl = `https://twitter.com/intent/tweet?url=${encodedUrl}&via=${via}&hashtags=${hashtags}`;
+    window.open(shareUrl, '_blank');
+  };
 
   const { data: game }: any = useReadContract({
     address: WNW_PRECOMPILE_ADDRESS,
@@ -83,16 +95,39 @@ export default function Page() {
 
   return (
     <div className="mt-10 max-h-screen space-y-6 overflow-y-auto p-4 md:p-8">
-      <h1 className="mb-5 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl">
-        {gameTitle || 'Loading...'}
-      </h1>
+      <div className="flex w-4/6 justify-between">
+        <h1 className="mb-5 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl">
+          {gameTitle || 'Loading...'}
+        </h1>
+        <button onClick={shareTwitter}>
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_451_295)">
+              <path
+                d="M15 13.4C14.3667 13.4 13.8 13.65 13.3667 14.0416L7.425 10.5833C7.46667 10.3916 7.5 10.2 7.5 9.99996C7.5 9.79996 7.46667 9.60829 7.425 9.41663L13.3 5.99163C13.75 6.40829 14.3417 6.66663 15 6.66663C16.3833 6.66663 17.5 5.54996 17.5 4.16663C17.5 2.78329 16.3833 1.66663 15 1.66663C13.6167 1.66663 12.5 2.78329 12.5 4.16663C12.5 4.36663 12.5333 4.55829 12.575 4.74996L6.7 8.17496C6.25 7.75829 5.65833 7.49996 5 7.49996C3.61667 7.49996 2.5 8.61663 2.5 9.99996C2.5 11.3833 3.61667 12.5 5 12.5C5.65833 12.5 6.25 12.2416 6.7 11.825L12.6333 15.2916C12.5917 15.4666 12.5667 15.65 12.5667 15.8333C12.5667 17.175 13.6583 18.2666 15 18.2666C16.3417 18.2666 17.4333 17.175 17.4333 15.8333C17.4333 14.4916 16.3417 13.4 15 13.4ZM15 3.33329C15.4583 3.33329 15.8333 3.70829 15.8333 4.16663C15.8333 4.62496 15.4583 4.99996 15 4.99996C14.5417 4.99996 14.1667 4.62496 14.1667 4.16663C14.1667 3.70829 14.5417 3.33329 15 3.33329ZM5 10.8333C4.54167 10.8333 4.16667 10.4583 4.16667 9.99996C4.16667 9.54163 4.54167 9.16663 5 9.16663C5.45833 9.16663 5.83333 9.54163 5.83333 9.99996C5.83333 10.4583 5.45833 10.8333 5 10.8333ZM15 16.6833C14.5417 16.6833 14.1667 16.3083 14.1667 15.85C14.1667 15.3916 14.5417 15.0166 15 15.0166C15.4583 15.0166 15.8333 15.3916 15.8333 15.85C15.8333 16.3083 15.4583 16.6833 15 16.6833Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_451_295">
+                <rect width="20" height="20" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </button>
+      </div>
 
       <div className="flex space-x-6">
         <Badge className="text-F7F8F8 rounded-3xl bg-[#575757] p-1.5 px-5 text-xs">
           {tokenName}
         </Badge>
         <Badge className="text-F7F8F8 rounded-3xl bg-[#575757] p-1.5 px-5 text-xs">
-          Ends in {timeLeft}
+          {!game?.isEnded ? `Ends in ${timeLeft}` : 'End'}
         </Badge>
         <Badge className="text-F7F8F8 rounded-3xl bg-[#575757] p-1.5 px-5 text-xs">
           Event Date: {eventDate}
--- components/game-detail-vote.tsx
+++ components/game-detail-vote.tsx
@@ -27,10 +27,17 @@ export function GameDetailVote() {
   const [betUp, setBetUp] = useState<boolean | null>(null); // Up/Down 선택 상태
   const [amount, setAmount] = useState(''); // Input 필드에 입력된 숫자
   const [clicked, setClicked] = useState(false);
-  // const [tokenInfo, setTokenInfo] = useState<{
-  //   name: string;
-  //   image: string;
-  // } | null>(null);
+  const url = 'https://bnb-wnw.online/';
+  const text = `ADF referral share ${url}`;
+  const encodedText = encodeURIComponent(text);
+  const encodedUrl = encodeURIComponent(url);
+
+  const shareTwitter = () => {
+    const via = 'Wise and Weird';
+    const hashtags = 'Prediction,Price,BNB,BSC';
+    const shareUrl = `https://twitter.com/intent/tweet?url=${encodedUrl}&via=${via}&hashtags=${hashtags}`;
+    window.open(shareUrl, '_blank');
+  };
 
   const { data: game }: any = useReadContract({
     address: WNW_PRECOMPILE_ADDRESS,
@@ -45,15 +52,15 @@ export function GameDetailVote() {
     if (game) {
       const startPrice = Number(game.startPrice);
 
-      const initialPriceChange = (Math.random() * 2 - 1) * 0.01;
+      const initialPriceChange = (Math.random() * 3 - 1) * 0.01;
       const initialPrice = Math.max(startPrice * (1 + initialPriceChange), 0);
       setCurrentPrice(initialPrice);
 
       const intervalId = setInterval(() => {
-        const priceChange = (Math.random() * 2 - 1) * 0.01;
+        const priceChange = (Math.random() * 3 - 1) * 0.01;
         const newPrice = Math.max(startPrice * (1 + priceChange), 0);
         setCurrentPrice(newPrice);
-      }, 20000);
+      }, 5000);
 
       return () => clearInterval(intervalId);
     }
@@ -83,11 +90,6 @@ export function GameDetailVote() {
     });
     setClicked(true);
     localStorage.setItem(`buttonClicked_${key}`, 'true');
-  };
-  const handleClaim = () => {
-    // Claim 버튼 클릭 시 실행할 로직
-    // ...
-    console.log('Claim function executed');
   };
 
   if (!game) {
@@ -237,9 +239,7 @@ export function GameDetailVote() {
           </div>
         </div>
         {totalPoolAmount === BigInt(0) ? (
-          <div className="text-center text-gray-500">
-            No votes yet, be the first to vote!
-          </div>
+          <div className="text-center text-gray-500">Be the first party!</div>
         ) : (
           <>
             <ResponsiveContainer width="100%" height={36}>
@@ -300,10 +300,33 @@ export function GameDetailVote() {
       </CardHeader>
 
       <CardContent className="max-h-auto mx-auto flex w-full max-w-sm flex-col gap-5 rounded-xl bg-white p-6 text-black">
-        <div className=" justify-between">
-          <div className="flex items-center text-2xl font-bold">Predict</div>
+        <div className="flex flex-row justify-between">
           <div>
+            <div className="flex items-center text-2xl font-bold">Predict</div>
             <div className="text-sm font-bold">Ended Price would be...</div>
+          </div>
+          <div>
+            <button onClick={shareTwitter}>
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_451_295)">
+                  <path
+                    d="M15 13.4C14.3667 13.4 13.8 13.65 13.3667 14.0416L7.425 10.5833C7.46667 10.3916 7.5 10.2 7.5 9.99996C7.5 9.79996 7.46667 9.60829 7.425 9.41663L13.3 5.99163C13.75 6.40829 14.3417 6.66663 15 6.66663C16.3833 6.66663 17.5 5.54996 17.5 4.16663C17.5 2.78329 16.3833 1.66663 15 1.66663C13.6167 1.66663 12.5 2.78329 12.5 4.16663C12.5 4.36663 12.5333 4.55829 12.575 4.74996L6.7 8.17496C6.25 7.75829 5.65833 7.49996 5 7.49996C3.61667 7.49996 2.5 8.61663 2.5 9.99996C2.5 11.3833 3.61667 12.5 5 12.5C5.65833 12.5 6.25 12.2416 6.7 11.825L12.6333 15.2916C12.5917 15.4666 12.5667 15.65 12.5667 15.8333C12.5667 17.175 13.6583 18.2666 15 18.2666C16.3417 18.2666 17.4333 17.175 17.4333 15.8333C17.4333 14.4916 16.3417 13.4 15 13.4ZM15 3.33329C15.4583 3.33329 15.8333 3.70829 15.8333 4.16663C15.8333 4.62496 15.4583 4.99996 15 4.99996C14.5417 4.99996 14.1667 4.62496 14.1667 4.16663C14.1667 3.70829 14.5417 3.33329 15 3.33329ZM5 10.8333C4.54167 10.8333 4.16667 10.4583 4.16667 9.99996C4.16667 9.54163 4.54167 9.16663 5 9.16663C5.45833 9.16663 5.83333 9.54163 5.83333 9.99996C5.83333 10.4583 5.45833 10.8333 5 10.8333ZM15 16.6833C14.5417 16.6833 14.1667 16.3083 14.1667 15.85C14.1667 15.3916 14.5417 15.0166 15 15.0166C15.4583 15.0166 15.8333 15.3916 15.8333 15.85C15.8333 16.3083 15.4583 16.6833 15 16.6833Z"
+                    fill="#323232"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_451_295">
+                    <rect width="20" height="20" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
           </div>
         </div>
 
@@ -358,31 +381,30 @@ export function GameDetailVote() {
               ? 'cursor-not-allowed bg-gray-400'
               : 'bg-[#E9B603] hover:shadow-lg active:scale-95 active:bg-gray-200'
           }`}
-          onClick={game.isEnded ? handleClaim : handleBet}
-          disabled={clicked && !game.isEnded}
+          onClick={handleBet}
+          disabled={clicked}
         >
-          {game.isEnded ? 'Claim' : clicked ? 'Not ended' : 'Confirm'}
+          {game.isEnded ? 'Game Ended' : clicked ? 'Not ended' : 'Confirm'}
         </button>
       </CardContent>
-      <button className='text-bold bg-white rounded'
-          onClick={() => {
-            if (currentPrice !== null) {
-              writeContract({
-                abi: WNW_ABI,
-                address: WNW_PRECOMPILE_ADDRESS,
-                functionName: 'endGame',
-                args: [
-                  game.gameId,
-                  currentPrice.toFixed(0)
-                ]
-              });
-            } else {
-              console.log("Current price is not available yet");
-            }
-          }}
-        >
-          End game
-        </button>
+      <button
+        className="text-bold rounded bg-white"
+        onClick={() => {
+          if (currentPrice !== null) {
+            console.log(game.gameId, currentPrice.toFixed(0));
+            writeContract({
+              abi: WNW_ABI,
+              address: WNW_PRECOMPILE_ADDRESS,
+              functionName: 'endGame',
+              args: [game.gameId, currentPrice.toFixed(0)]
+            });
+          } else {
+            console.log('Current price is not available yet');
+          }
+        }}
+      >
+        End game
+      </button>
     </Card>
   );
 }
