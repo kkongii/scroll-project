4ad6b1003e60c76a665d9c0bd9e2854d21990dbb
soaryong <soaryong.c@gmail.com>
2024-08-28 20:52:58 +0900

    twitter tag

--- app/(wnw)/layout.tsx
+++ app/(wnw)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/layout/header';
 import type { Metadata } from 'next';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Wise and Weird',
@@ -13,6 +14,24 @@ export default function DashboardLayout({
 }) {
   return (
     <>
+      <Head>
+        <title>Wise And Weird</title>
+        <meta
+          name="description"
+          content="Get precise news and predict the price"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@web3BlueNode" />
+        <meta name="twitter:title" content="Wise And Weird" />
+        <meta
+          name="twitter:description"
+          content="Predict price movement and profit from the community powered official events, making precise decisions"
+        />
+        <meta
+          name="twitter:image"
+          content="https://bnb-wnw.online/twitter-img.png"
+        />
+      </Head>
       <Header />
       <div className="flex h-screen overflow-hidden">
         <main className="flex-1 overflow-hidden pt-20">
