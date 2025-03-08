d5be4f7dff848a135b814ccfcbb29065acab5c69
leejaewon <ice9466@naver.com>
2025-03-09 02:44:23 +0900

    fix front 1

--- app/(wnw)/layout.tsx
+++ app/(wnw)/layout.tsx
@@ -1,24 +1,26 @@
-import Header from '@/components/layout/header';
-import type { Metadata } from 'next';
+import Header from '@/components/layout/header'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Wise and Weird',
-  description: 'Wise and Weird - Prediction Market'
-};
+  description: 'Wise and Weird - Prediction Market',
+}
 
 export default function DashboardLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <>
       <Header />
-      <div className="flex h-screen overflow-hidden">
-        <main className="flex-1 overflow-hidden pt-20">
+      {/* 배경색과 최소 높이 설정 */}
+      <div className="bg-[#faf3e0] min-h-screen">
+        {/* 만약 헤더 높이만큼 공간을 띄워주려면 pt-20 유지 */}
+        <main className="pt-20">
           <div className="container relative">{children}</div>
         </main>
       </div>
     </>
-  );
+  )
 }
